0b66077ad8c4ea9e359fcee3074d2042788d952a
niboo <moonevening8@gmail.com>
2022-09-08 21:27:17 +0900

    Add: footer 레이아웃 완성

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { FaRegSmileWink } from 'react-icons/fa';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex flex-col items-center max-w-100% h-300px bg-deep-gray mt-10px py-30px text-middle-gray">
+      <section className="flex flex-col items-center">
+        <p className="text-20px font-bold">© 2022 TO-DO-LIST</p>
+        <img className="w-20%" src="/images/to-do-list-logo.png" alt="logo" />
+      </section>
+      <section className="flex justify-between items-center w-20% h-60px">
+        <p>EMAIL</p>
+        <p>GITHUB</p>
+        <p>BLOG</p>
+      </section>
+      <section className="flex flex-col justify-between h-60px mt-20px">
+        <p className="text-18px font-bold">Niboo's Toy Project</p>
+        <span className="flex items-center justify-center">
+          <FaRegSmileWink />
+          <p className="ml-10px">NA EUN LEE</p>
+        </span>
+      </section>
+    </footer>
+  );
 };
 export default Footer;
